c95ca2bfd17116dcfc5729a97c882fd9084052f9
Michael Lin <michael.lin2k21@gmail.com>
2025-07-22 17:08:09 -0400

    fix distance filtering

--- filter_all.py
+++ filter_all.py
@@ -24,15 +24,21 @@ def filter_by_length(conf, signals, geodesics, skels, skel_ids):
     skel_ids = np.delete(skel_ids, mask, axis=0)
     return signals, geodesics, skels, skel_ids
 
-def skel_to_dist_mean(skel):
+def skel_to_dists(skel):
     p0, p1 = skel.vertices[0], skel.vertices[-1]
-    dists = list(map(lambda x: np.linalg.norm(np.cross(p1-p0,x-p0)/np.linalg.norm(p1-p0)), skel.vertices))
-    return np.mean(dists)
+    dists = list(map(lambda x: np.linalg.norm(np.cross(x-p0,x-p1)/np.linalg.norm(p1-p0)), skel.vertices))
+    return dists
+
+def skel_to_len(skel):
+    return np.linalg.norm(skel.vertices[-1]-skel.vertices[0])
 
 def filter_by_shape(conf, signals, geodesics, skels, skel_ids):
     with Pool(conf.num_cpus) as p:
-        means = list(p.map(skel_to_dist_mean, skels))
-    mask = [False if m < (g[-1] - g[0]) * conf.filter_all.thres_shape else True for m, g in zip(means, geodesics)]
+        dists = np.array(list(p.map(skel_to_dists, skels)))
+        lens = np.array(list(p.map(skel_to_len, skels)))
+    mask_mean = [False if m < l * conf.filter_all.thres_shape_mean else True for m, l in zip(np.mean(dists, axis=1), lens)]
+    mask_max = [False if np.max(d) < l * conf.filter_all.thres_shape_max else True for d, l in zip(dists, lens)] 
+    mask = [m1 & m2 for m1, m2 in zip(mask_mean, mask_max)]
     signals = np.delete(signals, mask, axis=1)
     geodesics = np.delete(geodesics, mask, axis=0)
     skels = np.delete(skels, mask, axis=0)
@@ -69,6 +75,8 @@ if __name__ == '__main__':
     extensions = ['.npz', '.h5']
     basenames = np.unique(sorted([get_basename(fname) for fname in glob(f'{output_path}/*') if os.path.splitext(fname)[1] in extensions]))
 
+    diff_list = []
+
     for basename in (pbar := tqdm(basenames)):
         pbar.set_description(basename)
         
@@ -79,10 +87,13 @@ if __name__ == '__main__':
         fiber_seg = readvol(os.path.join(output_path, basename + '-fiber_seg.h5'))
         cell_seg = readvol(os.path.join(output_path, basename + '-cell_seg.h5'))
 
-        #signals, geodesics, skels, skel_ids = filter_by_length(conf, signals, geodesics, skels, skel_ids)
-        #signals, geodesics, skels, skel_ids = filter_by_shape(conf, signals, geodesics, skels, skel_ids)
-        #signals, geodesics, skels, skel_ids = filter_by_cell(conf, signals, geodesics, skels, skel_ids, fiber_seg, cell_seg)
+        prior = len(geodesics)
+        signals, geodesics, skels, skel_ids = filter_by_length(conf, signals, geodesics, skels, skel_ids)
+        signals, geodesics, skels, skel_ids = filter_by_shape(conf, signals, geodesics, skels, skel_ids)
+        signals, geodesics, skels, skel_ids = filter_by_cell(conf, signals, geodesics, skels, skel_ids, fiber_seg, cell_seg)
         signals, geodesics, skels, skel_ids = filter_by_ratio(conf, signals, geodesics, skels, skel_ids)
+        post = len(geodesics)
+        diff_list.append([prior, post])
 
         np.savez(
             os.path.join(
@@ -102,3 +113,10 @@ if __name__ == '__main__':
             skels=skels,
             skel_ids=skel_ids
         )
+
+    print("\n"+"#"*25)
+    for idx, basename in enumerate(basenames):
+        prior, post = diff_list[idx]
+        diff = post/prior
+        print(f"{basename}: kept {post}/{prior} ({diff:04f}))")
+    print("#"*25+"\n")
